dcbcd6768757dee16016560d8b723e406c55e171
Louie <eliasdandouch@gmail.com>
2024-05-20 19:25:22 -0700

    Initial Commit

---
+++ NutriFit/NutriFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriFitApp.swift
+//  NutriFit
+//
+//  Created by Elias Dandouch on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct NutriFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
